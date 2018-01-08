7e75e01a8553ecad4ac24b720961128c2d79221d
Aliaksei Tsybulia <Aliaksei_Tsybulia@epam.com>
2018-01-08 17:27:14 +0300

    Tests update

--- src/test/java/com/epam/gomel/RestTest.java
+++ src/test/java/com/epam/gomel/RestTest.java
@@ -1,7 +1,14 @@
 package com.epam.gomel;
 
+import static java.time.Duration.ofMinutes;
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
@@ -13,32 +20,87 @@ import org.junit.jupiter.api.Tag;
 @Tag("RestTest")
 class RestTest {
 
+    @Disabled
+    @Test
+    @DisplayName("disabledTest")
+    void disabledTest() {
+        Response res = get("nonexistent URI");
+        assertEquals(200, res.getStatusCode());
+    }
+
     @Test
     @DisplayName("statusCodeTest")
     void statusCodeTest() {
-        given().
-            get("https://jsonplaceholder.typicode.com/posts/1").
-        then().
-            statusCode(200).
-            log().all();
+        Response res = get("https://jsonplaceholder.typicode.com/posts/1");
+        assertEquals(200, res.getStatusCode());
     }
 
     @Test
-    @DisplayName("bodyEqualTest")
-    void bodyEqualTest() {
-        given().
-                get("https://jsonplaceholder.typicode.com/posts/1").
-                then().
-                body("title",equalTo(
-                        "sunt aut facere repellat provident occaecati excepturi optio reprehenderit"));
+    @DisplayName("contentTypeTest")
+    void contentTypeTest() {
+        Response res = get("https://jsonplaceholder.typicode.com/posts/1");
+        assertEquals("application/json; charset=utf-8", res.getContentType());
     }
 
     @Test
-    @DisplayName("bodyHasItemsTest")
-    void bodyHasItemsTest() {
-        given().
-                get("https://jsonplaceholder.typicode.com/posts").
-                then().
-                body("id",hasItems(1, 2, 3));
+    @DisplayName("timeoutTest")
+    void timeoutTest() {
+        assertTimeout(ofMinutes(1), () -> {
+            get("https://jsonplaceholder.typicode.com/posts/1");
+        });
+    }
+
+    @Test
+    @DisplayName("bodyAssertionTest")
+    void bodyAssertionTest() {
+        Response res = get("https://jsonplaceholder.typicode.com/posts/1");
+        String json = res.asString();
+        JsonPath jp = new JsonPath(json);
+        assertEquals("sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+                jp.get("title"));
+    }
+
+    @Test
+    @DisplayName("bodyAssertionHamcrestTest")
+    void bodyAssertionHamcrestTest() {
+        Response res = get("https://jsonplaceholder.typicode.com/posts/1");
+        String json = res.asString();
+        JsonPath jp = new JsonPath(json);
+        assertThat(jp.get("userId"), equalTo(1));
+        assertThat(jp.get("id"), equalTo(1));
+    }
+
+    @Test
+    @DisplayName("bodyArrayHasItemsTest")
+    void bodyArrayHasItemsTest() {
+        Response res = get("https://jsonplaceholder.typicode.com/posts");
+        String json = res.asString();
+        JsonPath jp = new JsonPath(json);
+        assertThat(jp.get("id"), hasItems(1, 2, 100));
+    }
+
+    @Test
+    @DisplayName("ParameterTest")
+    void ParameterTest() {
+        Response res = given().
+                            param("id", 1, 2, 100).
+                            when().
+                            get("https://jsonplaceholder.typicode.com/posts");
+        String json = res.asString();
+        JsonPath jp = new JsonPath(json);
+        assertThat(jp.get("id"), hasItems(1, 2, 100));
+    }
+
+    @Test
+    @DisplayName("Parameter2Test")
+    void Parameter2Test() {
+        Response res = given().
+            param("id", 94).
+            param("title", "qui qui voluptates illo iste minima").
+        when().
+            get("https://jsonplaceholder.typicode.com/posts");
+        String json = res.asString();
+        JsonPath jp = new JsonPath(json);
+        assertThat(jp.get("userId"), hasItems(10));
     }
 }
