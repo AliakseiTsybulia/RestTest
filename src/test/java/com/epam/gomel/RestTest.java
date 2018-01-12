afeb21eb5ca8fb925e66095c6ca1395c20f005af
Aliaksei Tsybulia <Aliaksei_Tsybulia@epam.com>
2018-01-12 17:32:48 +0300

    Tests update

--- src/test/java/com/epam/gomel/RestTest.java
+++ src/test/java/com/epam/gomel/RestTest.java
@@ -2,8 +2,8 @@ package com.epam.gomel;
 
 import static java.time.Duration.ofMinutes;
 import static io.restassured.RestAssured.*;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import static org.junit.jupiter.api.Assertions.*;
-
 
 import com.jayway.jsonpath.Filter;
 import com.jayway.jsonpath.JsonPath;
@@ -29,6 +29,9 @@ import static com.jayway.jsonpath.Filter.filter;
 
 @Tag("RestTest")
 class RestTest {
+    private final String POSTS_URI = "http://jsonplaceholder.typicode.com/posts";
+    private final String COMMENTS_URI = "http://jsonplaceholder.typicode.com/comments";
+    private final String USERS_URI = "http://jsonplaceholder.typicode.com/users";
 
     @Disabled
     @Test
@@ -41,14 +44,14 @@ class RestTest {
     @Test
     @DisplayName("Status code test")
     void statusCodeTest() {
-        Response res = get("https://jsonplaceholder.typicode.com/posts/1");
+        Response res = get(POSTS_URI + "/1");
         assertEquals(200, res.getStatusCode());
     }
 
     @Test
     @DisplayName("Content type test")
     void contentTypeTest() {
-        Response res = get("https://jsonplaceholder.typicode.com/posts/1");
+        Response res = get(POSTS_URI + "/1");
         assertEquals("application/json; charset=utf-8", res.getContentType());
     }
 
@@ -56,112 +59,78 @@ class RestTest {
     @DisplayName("Timeout test")
     void timeoutTest() {
         assertTimeout(ofMinutes(1), () -> {
-            get("https://jsonplaceholder.typicode.com/posts/1");
+            get(POSTS_URI + "/1");
         });
     }
 
     @Test
     @DisplayName("Body assertion test")
     void bodyAssertionTest() {
-        Response res = get("https://jsonplaceholder.typicode.com/posts/1");
-        String json = res.asString();
+        Response res = get(POSTS_URI + "/1");
         assertEquals("sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-                JsonPath.read(json, "$.title"));
+                JsonPath.read(res.asString(), "$.title"));
     }
 
     @Test
     @DisplayName("Body assertion test 2")
     void bodyAssertion2Test() {
-        Response res = get("https://jsonplaceholder.typicode.com/posts");
-        String json = res.asString();
-        List<String> id = JsonPath.read(json, "$.[?(@.id == 3)].id");
-        assertTrue(id.contains(3));
+        Response res = get(POSTS_URI + "?id=3");
+        Integer id = JsonPath.read(res.asString(), "$.[0].id");
+        assertTrue(id.equals(3));
     }
 
     @Test
     @DisplayName("Body assertion test 3")
     void bodyAssertion3Test() {
-        Response res = get("https://jsonplaceholder.typicode.com/posts");
-        String json = res.asString();
-        int id = JsonPath.parse(json).read( "$.[2].id");
+        Response res = get(POSTS_URI);
+        int id = JsonPath.parse(res.asString()).read( "$.[2].id");
         assertEquals(3, id);
     }
 
     @Test
     @DisplayName("Body assertion test 4")
     void bodyAssertion4Test() {
-        Response res = get("https://jsonplaceholder.typicode.com/posts");
-        String json = res.asString();
-        List<Integer> id = JsonPath.read(json, "$.[*].id");
+        Response res = get(POSTS_URI);
+        List<Integer> id = JsonPath.read(res.asString(), "$.[*].id");
         assertTrue(id.contains(3));
     }
 
     @Test
-    @DisplayName("Parameter test")
-    void ParameterTest() {
-        Response res = given().
-                param("id", 1, 2, 100).
-                when().
-                get("https://jsonplaceholder.typicode.com/posts");
-        String json = res.asString();
-        List<Integer> id = JsonPath.read(json, "$.[*].id");
-        assertTrue(id.contains(2));
-    }
-
-    @Test
-    @DisplayName("Parameter test 2")
-    void Parameter2Test() {
-        Response res = given().
-                param("id", 94).
-                param("title", "qui qui voluptates illo iste minima").
-                when().
-                get("https://jsonplaceholder.typicode.com/posts");
-        String json = res.asString();
-        List<Integer> userId = JsonPath.read(json, "$.[*].userId");
-        assertTrue(userId.contains(10));
-
-    }
-
-    @Test
-    @DisplayName("Filter inline predicates test")
-    void filterInlinePredicatesTest() {
-        Response res = get("https://jsonplaceholder.typicode.com/posts");
-        String json = res.asString();
-        List<Map<String, Object>>  objects =  JsonPath.parse(json)
+    @DisplayName("Body assertion test 5 with filtering inline predicates")
+    void bodyAssertion5Test() {
+        Response res = get(POSTS_URI);
+        List<Map<String, Object>>  objects =  JsonPath.parse(res.asString())
                 .read("$.[?(!(@.userId == 1 && @.id <= 11))]");
         assertTrue(objects.get(3).get("id").equals(14));
     }
 
     @Test
-    @DisplayName("Filter predicates test")
-    void filterPredicatesTest() {
-        Response res = get("https://jsonplaceholder.typicode.com/posts");
-        String json = res.asString();
+    @DisplayName("Body assertion test 6 with filtering predicates")
+    void bodyAssertion6Test() {
+        Response res = get(POSTS_URI);
         Filter filter = filter(
                 where("userId").ne(1).and("id").gt(10)
         );
-        List<Map<String, Object>>  objects =  JsonPath.parse(json)
+        List<Map<String, Object>>  objects =  JsonPath.parse(res.asString())
                 .read("$.[?]", filter);
         assertTrue(objects.get(3).get("id").equals(14));
     }
 
     @Test
-    @DisplayName("Custom predicates test")
-    void customPredicatesTest() {
-        Response res = get("https://jsonplaceholder.typicode.com/posts");
-        String json = res.asString();
-        ReadContext context = JsonPath.parse(json);
+    @DisplayName("Body assertion test 7 with custom filtering predicates")
+    void bodyAssertion7Test() {
+        Response res = get(POSTS_URI);
+        ReadContext context = JsonPath.parse(res.asString());
         Predicate customFilterPredicate = context1 -> context1.item(Map.class).containsKey("id");
         List<Map<String, Object>>  objects =  context.read("$.[?]", List.class, customFilterPredicate);
         assertTrue(objects.get(3).get("id").equals(4));
     }
 
     @Test
-    @DisplayName("Mixed filter predicates test")
-    void mixedFilterPredicates1Test() {
-        Response res = get("https://jsonplaceholder.typicode.com/posts");
-        String json = res.asString();
-        ReadContext context = JsonPath.parse(json);
+    @DisplayName("Body assertion test 8 with mixed filtering predicates")
+    void bodyAssertion8Test() {
+        Response res = get(POSTS_URI);
+        ReadContext context = JsonPath.parse(res.asString());
         Predicate customFilterPredicate = context1 -> context1.item(Map.class).containsKey("id");
         Filter filter = filter(
                 where("id").eq(14)
@@ -171,11 +140,10 @@ class RestTest {
     }
 
     @Test
-    @DisplayName("Mixed filter predicates test 2 with /comments resource")
-    void mixedFilterPredicates2Test() {
-        Response res = get("https://jsonplaceholder.typicode.com/comments");
-        String json = res.asString();
-        ReadContext context = JsonPath.parse(json);
+    @DisplayName("Body assertion test 9 with mixed filtering predicates")
+    void bodyAssertion9Test() {
+        Response res = get(COMMENTS_URI);
+        ReadContext context = JsonPath.parse(res.asString());
         Predicate customFilterPredicate = context1 -> context1.item(Map.class).containsKey("email");
         Filter filter = filter(
                 where("name").eq("modi ut eos dolores illum nam dolor")
@@ -185,11 +153,10 @@ class RestTest {
     }
 
     @Test
-    @DisplayName("Mixed filter predicates test 3 with /comments resource")
-    void mixedFilterPredicates3Test() {
-        Response res = get("https://jsonplaceholder.typicode.com/comments");
-        String json = res.asString();
-        ReadContext context = JsonPath.parse(json);
+    @DisplayName("Body assertion test 10 with mixed filtering predicates")
+    void bodyAssertion10Test() {
+        Response res = get(COMMENTS_URI);
+        ReadContext context = JsonPath.parse(res.asString());
         Predicate customFilterPredicate = context1 -> context1.item(Map.class).containsKey("email");
         Filter filter1 = filter(
                 where("name").eq("modi ut eos dolores illum nam dolor")
@@ -206,11 +173,10 @@ class RestTest {
     }
 
     @Test
-    @DisplayName("Mixed filter predicates test 4 with /users resource")
-    void mixedFilterPredicates4Test() {
-        Response res = get("https://jsonplaceholder.typicode.com/users");
-        String json = res.asString();
-        ReadContext context = JsonPath.parse(json);
+    @DisplayName("Body assertion test 11 with mixed filtering predicates")
+    void bodyAssertion11Test() {
+        Response res = get(USERS_URI);
+        ReadContext context = JsonPath.parse(res.asString());
         Predicate customFilterPredicate = context1 -> context1.item(Map.class).containsKey("id");
         Filter filter1 = filter(
                 where("name").eq("Leanne Graham")
@@ -227,13 +193,40 @@ class RestTest {
     }
 
     @Test
-    @DisplayName("Function using test with /users resource")
-    void functionUsingTest() {
-        Response res = get("https://jsonplaceholder.typicode.com/users");
-        String json = res.asString();
-        ReadContext context = JsonPath.parse(json);
+    @DisplayName("Body assertion test 12 with filtering function")
+    void bodyAssertion12Test() {
+        Response res = get(USERS_URI);
+        ReadContext context = JsonPath.parse(res.asString());
         List<Integer> ids = context.read("$.[?(@.name == \"Leanne Graham\")]." +
                 "address[?(@.city == \"Gwenborough\")].geo[?(@.lat == \"-37.3159\")].length()");
         assertTrue(ids.get(0).equals(2));
+    }
+
+    @Test
+    @DisplayName("JSON schema test")
+    void jsonSchemaTest() {
+        given().
+            get("http://jsonplaceholder.typicode.com/users/1").
+        then().
+            assertThat().body(matchesJsonSchemaInClasspath("schema.json"));
+    }
+
+    @Test
+    @DisplayName("Posts with users connection test")
+    void connection1Test() {
+        List<Integer> postUserIds = JsonPath.parse(get(POSTS_URI).asString()).read("$.[*].userId");
+        List<Integer> userIds = JsonPath.parse(get(USERS_URI).asString()).read("$.[*].id");
+
+        assertTrue(userIds.containsAll(postUserIds));
+    }
+
+    @Test
+    @DisplayName("Posts with comments connection test")
+    void connection2Test() {
+        List<Integer> postIds = JsonPath.parse(get(POSTS_URI).asString()).read("$.[*].id");
+        List<Integer> commentsPostIds = JsonPath.parse(get(USERS_URI).asString()).
+                read("$.[*].postId");
+
+        assertTrue(postIds.containsAll(commentsPostIds));
     }
 }
